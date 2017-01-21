6e5fa5f9d32a21459d255a541d45cf5335868190
charbergman <charlotte.bergman@live.com>
2017-01-21 12:18:23 -0800

    Stuff

---
+++ Composer/Assets/ScoreKeep.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreKeep : MonoBehaviour {
+    float score;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        score = Time.deltaTime;
+	}
+    void OnGUI()
+    {
+        Text text = GetComponentInChildren<Text>();
+        text.text = ""+ score;
+    }
+}
