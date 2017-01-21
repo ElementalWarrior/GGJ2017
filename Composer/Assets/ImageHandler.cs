aaaa45535805aadc3694e7ca0cc8279e7258311e
James McDonnell <topgamer7@gmail.com>
2017-01-21 11:38:02 -0800

    Adding ash's comic's to the resources folder.

---
+++ Composer/Assets/ImageHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp(KeyCode.Space))
+        {
+            Sprite tex = Resources.Load<Sprite>("comic_02");
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = tex;
+        }
+	}
+}
