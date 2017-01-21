46864c885c7cb7e06365141d69d2c59324e2590d
James McDonnell <topgamer7@gmail.com>
2017-01-20 19:38:28 -0800

    Removing library and adding assets

---
+++ Composer/Assets/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        GameObject _note = GameObject.Instantiate(GameObject.Find("Note"));
+        _note.GetComponent<MovementHandler>().transform.position = new Vector2(0, 720 / 2);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
