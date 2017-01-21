34a4e5d9c672b46a97f6a730993abf32534b7b7a
James McDonnell <topgamer7@gmail.com>
2017-01-20 20:24:08 -0800

    Having notes move in all directions

--- Composer/Assets/GameManager.cs
+++ Composer/Assets/GameManager.cs
@@ -4,11 +4,39 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
 
-        GameObject _note = GameObject.Instantiate(GameObject.Find("Note"));
-        _note.GetComponent<MovementHandler>().transform.position = new Vector2(0, 720 / 2);
+    GameObject defaultNote;
+    // Use this for initialization
+    void Start () {
+        defaultNote = GameObject.Find("Note");
+        defaultNote.SetActive(false);
+        CloneNote(MovementHandler.Tracks.Down);
+        CloneNote(MovementHandler.Tracks.Left);
+        CloneNote(MovementHandler.Tracks.Up);
+        CloneNote(MovementHandler.Tracks.Right);
+        //_note.GetComponent<MovementHandler>().transform.position = ;
+    }
+    public void CloneNote(MovementHandler.Tracks direction)
+    {
+        Vector3 pos = new Vector3(0f,0f,0f);
+        switch(direction)
+        {
+            case MovementHandler.Tracks.Up:
+                pos = new Vector3(1280/2, 720f, 10f);
+                break;
+            case MovementHandler.Tracks.Right:
+                pos = new Vector3(0, 720 /2, 10f);
+                break;
+            case MovementHandler.Tracks.Down:
+                pos = new Vector3(1280 / 2, 0f, 10f);
+                break;
+            case MovementHandler.Tracks.Left:
+                pos = new Vector3(1280, 720/2, 10f);
+                break;
+        }
+        GameObject _note = GameObject.Instantiate(defaultNote, Camera.main.ScreenToWorldPoint(pos), new Quaternion());
+        _note.SetActive(true);
+        _note.GetComponent<MovementHandler>().Track = direction;
     }
 	
 	// Update is called once per frame
--- Composer/Assets/MovementHandler.cs
+++ Composer/Assets/MovementHandler.cs
@@ -13,8 +13,25 @@ public class MovementHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position += new Vector3(1, 0, 0);
+        Vector3 direction = new Vector3(0f, 0f, 0f);
+        switch(Track)
+        {
+            case Tracks.Up:
+                direction.y = -0.1f;
+                break;
+            case Tracks.Right:
+                direction.x = 0.1f;
+                break;
+            case Tracks.Down:
+                direction.y = 0.1f;
+                break;
+            case Tracks.Left:
+                direction.x = -0.1f;
+                break;
+        }
+        gameObject.transform.position += direction;
     }
-
-    public int Track { get; set; }
+    
+    public enum Tracks { Up, Right, Down, Left} 
+    public Tracks Track { get; set; }
 }
