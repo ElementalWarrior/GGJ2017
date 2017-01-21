04705a08eafd7b7e159465f3fa72f6e80a838e6a
ashwongg <ashwong1994@gmail.com>
2017-01-21 14:52:12 -0800

    Leftover merge

--- Composer/Assets/GameManager.cs
+++ Composer/Assets/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 
 
-    GameObject defaultNote;
+    public static GameObject defaultNote;
     public static GameObject ObjectDeath;
     // Use this for initialization
     void Start () {
@@ -16,43 +16,46 @@ public class GameManager : MonoBehaviour {
         //CloneNote(MovementHandler.Tracks.Up);
         //CloneNote(MovementHandler.Tracks.Right);
     }
-    public void CloneNote(MovementHandler.Tracks direction)
+    public enum NoteColor { Blue, Green, Red, Yellow }
+    public static GameObject CloneNote(Vector2 Direction, GameObject spawner,  NoteColor color)
     {
-        Vector3 pos = new Vector3(0f,0f,0f);
-        switch(direction)
+        GameObject newNote = Instantiate(GameManager.defaultNote, spawner.transform.position, new Quaternion());
+        switch (color)
         {
-            case MovementHandler.Tracks.Up:
-                pos = new Vector3(1280/2, 720f, 10f);
+            case NoteColor.Blue:
+                newNote.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("note_blue");
                 break;
-            case MovementHandler.Tracks.Right:
-                pos = new Vector3(0, 720 /2, 10f);
+            case NoteColor.Green:
+                newNote.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("note_green");
                 break;
-            case MovementHandler.Tracks.Down:
-                pos = new Vector3(1280 / 2, 0f, 10f);
+            case NoteColor.Red:
+                newNote.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("note_red");
                 break;
-            case MovementHandler.Tracks.Left:
-                pos = new Vector3(1280, 720/2, 10f);
+            case NoteColor.Yellow:
+                newNote.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("note_yellow");
                 break;
         }
-        GameObject _note = GameObject.Instantiate(defaultNote, Camera.main.ScreenToWorldPoint(pos), new Quaternion());
-        _note.SetActive(true);
-        _note.GetComponent<MovementHandler>().Track = direction;
+        //newNote.transform.position = Vector2.zero;
+        newNote.SetActive(true);
+        MovementHandler mhNote = newNote.GetComponent<MovementHandler>();
+        mhNote.direction = Direction;
+        return newNote;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyUp(KeyCode.LeftArrow))
-        {
-            CloneNote(MovementHandler.Tracks.Left);
-        }else if(Input.GetKeyUp(KeyCode.RightArrow))
-        {
-            CloneNote(MovementHandler.Tracks.Right);
-        } else if (Input.GetKeyUp(KeyCode.UpArrow))
-        {
-            CloneNote(MovementHandler.Tracks.Down);
-        } else if (Input.GetKeyUp(KeyCode.DownArrow))
-        {
-            CloneNote(MovementHandler.Tracks.Up);
-        }
+        //if (Input.GetKeyUp(KeyCode.LeftArrow))
+        //{
+        //    CloneNote(MovementHandler.Tracks.Left);
+        //}else if(Input.GetKeyUp(KeyCode.RightArrow))
+        //{
+        //    CloneNote(MovementHandler.Tracks.Right);
+        //} else if (Input.GetKeyUp(KeyCode.UpArrow))
+        //{
+        //    CloneNote(MovementHandler.Tracks.Down);
+        //} else if (Input.GetKeyUp(KeyCode.DownArrow))
+        //{
+        //    CloneNote(MovementHandler.Tracks.Up);
+        //}
     }
 }
---
+++ Composer/Assets/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    public Vector2 Direction = Vector2.zero;
+    public float WaitTime = 1;
+    public float StartTime = 5;
+    public bool running = true;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnQueue());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    IEnumerator SpawnQueue()
+    {
+        yield return new WaitForSeconds(StartTime);
+        while (running)
+        {
+            
+            GameManager.CloneNote(Direction, this.gameObject, (GameManager.NoteColor)Random.Range(0, 4));
+            
+            WaitTime = Mathf.Max(WaitTime - 0.01f, 0.1f);
+            yield return new WaitForSeconds(WaitTime);
+        }
+        yield return null;
+    }
+}
