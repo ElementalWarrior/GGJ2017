11d8dc06826977bf80df2664eb645ffde51794d0
James McDonnell <topgamer7@gmail.com>
2017-01-21 18:49:07 -0800

    Making bounds scale depending on game size.

--- Composer/Assets/GameManager.cs
+++ Composer/Assets/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,16 +16,21 @@ public class GameManager : MonoBehaviour {
     void Start () {
         ObjectDeath = GameObject.Find("CollisionDeath");
         ComposerColor = GameObject.Find("ComposerBackground").GetComponent<ComposerColor>();
+        BoxCollider2D collider = this.GetComponent<BoxCollider2D>();
+        
+        //resize camera bounds so we remove waves after they leave the viewport
+        collider.size = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelRect.width, Camera.main.pixelRect.height, 0)) - Camera.main.ScreenToWorldPoint(new Vector3(0,0,0));
+
     }
     public enum NoteColor { Blue, Green, Red, Yellow }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow))// || Input.GetAxis("Horizontal") < -0.5)
         {
             WaveSpawnerLeft.Spawn(ComposerColor.CurrentColor);
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        else if (Input.GetKeyDown(KeyCode.RightArrow))// || Input.GetAxis("Horizontal") > 0.5)
         {
             WaveSpawnerRight.Spawn(ComposerColor.CurrentColor);
         }
